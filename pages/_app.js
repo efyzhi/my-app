47328a61a90c19e6da67f28489657701b1e3ef2f
efyzhi <pelurobert10@gmail.com>
2023-04-05 23:17:12 +0100

    update

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '@/component/styles/globals.css'
 // import navMenu from "../src/comps/navMenu"
 import Header from "../src/comps/Header"
 import Layout from '../src/comps/Layout'
-
+import MobileNav from '../src/comps/MobileNav'
 
 
 export default function App({ Component, pageProps }) {
@@ -11,6 +11,7 @@ export default function App({ Component, pageProps }) {
     <div>
       <Layout>
       <Header />
+      <MobileNav />
       <Component {...pageProps} />
       </Layout>
     </div>
--- src/comps/MobileNav.js
+++ src/comps/MobileNav.js
@@ -1,19 +1,34 @@
 import { react, useState, useEffect } from "react" 
 import Link from 'next/link'
+import { FaBars, FaTimes } from 'react-icons/fa'
 import { BiHomeAlt, BiUser} from "react-icons/bi";
 import { BsClipboardData, BsBriefcase, BsChatSquare } from "react-icons/bs"
-import styles from "styles/Layout.module.css"
+import styles from "styles/MobileNav.module.css"
 
 
-const Layout = ({ children }) => {
+const MobileNav = ({ children }) => {
+  const  [sideBar, setSideBar] = useState(false)
   const [activNav, setActivNav] = useState(false)
+const handleSideBar = () => {
+  setSideBar(!sideBar)
+}
 
   return (
     <main>
     <div className={styles.container}>
-      <div> {children} </div>   
       
-    <nav className={styles.navMenu}>
+      <div> {children} </div>
+      
+      
+     <div > 
+     {/* <div className={!sideBar ? styles.sideBar .active : styles.sideBar } onClick={handleSideBar}> */}
+      {/* </div> */}
+     {/* { <nav className={styles.navMenu} onClick={handleSideBar}>
+
+     } */}
+  </div> 
+    { sideBar && ( <nav className={styles.MobilnavMenu } onClick={handleSideBar}>
+        <div className={styles.mbg}>
              <Link href="/" passHref  smooth={true}
          spy={true} > <BiHomeAlt onClick={() => setActivNav(!activNav)} className={activNav === "/" ? styles.active : styles }  /></Link>
         
@@ -25,9 +40,14 @@ const Layout = ({ children }) => {
         
         
         <Link href="/contact" onClick={() => setActivNav("/contact")} className={activNav === "/contact" ? "active" : "" } > <BsChatSquare className={styles.icons} /></Link>
+        </div>
       </nav> 
+  )
+      }
+        <button className={styles.menu} onClick={handleSideBar} >  { !sideBar ? <FaBars/> : <FaTimes/> }   
+        </button>
     </div>    
     </main>
   )
 }
-export default Layout
+export default MobileNav
