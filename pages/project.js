d214e8b6d7214d837ca9a634fd462228afa6bc4a
efyzhi <pelurobert10@gmail.com>
2023-04-07 18:03:33 +0100

    addition

--- pages/project.js
+++ pages/project.js
@@ -1,17 +1,28 @@
 // import proj from "../images/proj.jpg"
 import styles from "styles/Layout.module.css"
 
+export const getStaticProps = async () => {
+  const response = await fetch('https://api.github.com/users/efyzhi/repos') 
+  const data = await response.json()
 
-const Service = () => {
+  return {
+    props: { repos: data }
+  }
+}
+
+const Service = ({ repos }) => {
   return (
     <>
+    <div className={styles.projBox}>
       <h1> Projects Page </h1>
-      <div className="proj-container">
-        <img  className="proj-img"/>
-        <h3> title: Proj 1 </h3>
-        <h3> Proj description: </h3>
-        <h3> github link</h3>
-        <h3> live demo</h3>
+      <div >
+        {repos.map(repos => (
+          <div key={repos.id}> 
+            <h3>{ repos.name }</h3>
+            < img src={repos.owner.avatar_url} className={styles.projImage}/>
+          </div> 
+        ))}
+      </div>
       </div>
     </>
   )
